4f797f4f819c697bf130c93b30ef424065195205
csd-03 <csd-03@GAMAL-PC>
2016-05-11 17:17:51 +0200

    Initial Commit2

--- src/project/ComplexMethod.java
+++ src/project/ComplexMethod.java
@@ -3,15 +3,26 @@ package project;
 public class ComplexMethod {
 
     public String intToEnglishValue(int i) {
-        switch(i){
-			case 1: return "One";
-			case 2: return "Two";
-			case 3: return "Three";
-			case 4: return "Four";
-			case 5: return "Five";
-			case 6: return "Six";
-			default:
-			throw new NotImplementedException();
-		}
+        if(i==1){
+            return "One";
+        }
+        if(i==2){
+            return "One";
+        }
+        if(i==3){
+            return "One";
+        }
+        if(i==4){
+            return "One";
+        }if(i==5){
+            return "One";
+        }
+        if(i==6){
+            return "One";
+        }
+        return "Test";
+        
+    }
+
 
 }
